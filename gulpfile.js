4e601a56cf0f4db6f9a981041501af8c7563338d
hellowillian21 <2544971181@qq.com>
2018-08-15 11:43:20 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var rev = require('gulp-rev'); //给每个文件计算出一个哈希码 修改文件名以区别版本
+var revReplace = require('gulp-rev-replace');//更新文件的引用 例如index.html引用js
+var useref = require('gulp-useref'); //自动合并文件 例如把几个js文件合并在一起
+var filter = require('gulp-filter');
+var uglify = require('gulp-uglify'); //压缩js的插件
+var csso = require('gulp-csso');  //压缩css的插件
+
+gulp.task('default',function(){
+    var jsFilter = filter('**/*.js',{restore:true});
+    var cssFilter = filter('**/*.css',{restore:true});
+    var indexHtmlFilter = filter(['**/*','!**/index.html'],{restore:true});
+
+    return gulp.src('src/index.html')
+        .pipe(useref())
+        .pipe(jsFilter)
+        .pipe(uglify())
+        .pipe(jsFilter.restore)
+        //此处压缩css报错未完善 
+        // .pipe(cssFilter)
+        // .pipe(csso())
+        // .pipe(cssFilter.restore)
+        .pipe(indexHtmlFilter)
+        .pipe(rev())
+        .pipe(indexHtmlFilter.restore)
+        .pipe(revReplace())
+        .pipe(gulp.dest('dist'));
+});
